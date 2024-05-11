02b435e249948c18a791a5af06217a538189add9
Yussouf <Yussoufb@gmail.com>
2024-05-11 13:16:51 -0400

    change

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "server1" {
   ami                    = "ami-0d7a109bf30624c99"
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg1.id]
   availability_zone      = "us-east-1a"
   subnet_id              = aws_subnet.private1.id
@@ -12,7 +12,7 @@ resource "aws_instance" "server1" {
 }
 resource "aws_instance" "server2" {
   ami                    = "ami-0d7a109bf30624c99"
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg1.id]
   availability_zone      = "us-east-1b"
   subnet_id              = aws_subnet.private2.id
